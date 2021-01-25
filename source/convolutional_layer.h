09ef17f09df0f764959af6cea3e1a9ef06f17066
Seungtae <stnamjef@naver.com>
2021-01-25 15:36:01 +0900

    forward, backward function modified

--- source/convolutional_layer.h
+++ source/convolutional_layer.h
@@ -18,11 +18,13 @@ namespace simple_nn
 		int pad;
 		int out_block_size;
 		int ker_block_size;
+		int work_space_size;
 		string init_opt;
 		float* kernel;
 		float* dkernel;
 		float* bias;
 		float* dbias;
+		float* work_space;
 	public:
 		Conv2d(int out_channels,
 			int kernel_size,
@@ -61,6 +63,7 @@ namespace simple_nn
 		pad(pad),
 		out_block_size(0),
 		ker_block_size(0),
+		work_space_size(0),
 		init_opt(init_opt)
 	{
 		if (init_opt != "normal" && init_opt != "uniform") {
@@ -85,6 +88,7 @@ namespace simple_nn
 		pad(pad),
 		out_block_size(0),
 		ker_block_size(0),
+		work_space_size(0),
 		init_opt(init_opt)
 	{
 		if (init_opt != "normal" && init_opt != "uniform") {
@@ -100,6 +104,7 @@ namespace simple_nn
 		delete_memory(dkernel);
 		delete_memory(bias);
 		delete_memory(dbias);
+		delete_memory(work_space);
 	}
 
 	void Conv2d::set_layer(int batch, const vector<int>& input_shape)
@@ -112,6 +117,7 @@ namespace simple_nn
 		out_w = calc_outsize(in_w, ker_w, 1, pad);
 		out_block_size = batch * out_chs * out_h * out_w;
 		ker_block_size = out_chs * in_chs * ker_h * ker_w;
+		work_space_size = in_chs * ker_h * ker_w * out_h * out_w;
 
 		allocate_memory(output, out_block_size);
 		allocate_memory(delta, out_block_size);
@@ -119,6 +125,7 @@ namespace simple_nn
 		allocate_memory(dkernel, ker_block_size);
 		allocate_memory(bias, out_chs);
 		allocate_memory(dbias, out_chs);
+		allocate_memory(work_space, work_space_size);
 
 		init_weight(kernel, ker_block_size, in_h * in_w * in_chs, out_h * out_w, init_opt);
 		set_zero(dkernel, ker_block_size);
@@ -154,64 +161,59 @@ namespace simple_nn
 		}
 	}
 
-	void add_bias(float* C, int N, float bias)
+	void add_bias(float* C, int M, int N, const float* bias)
 	{
-		std::for_each(C, C + N, [&](float& elem) { elem += bias; });
+		for (int i = 0; i < M; i++) {
+			for (int j = 0; j < N; j++) {
+				C[j + N * i] += bias[i];
+			}
+		}
 	}
 
 	void Conv2d::forward_propagate(const float* prev_out, bool isPrediction)
 	{
 		/*
-		* A: M x K = 1 x (ker_h * ker_w * in_channels)
+		* A: M x K = out_chs x (ker_h * ker_w * in_channels)
 		* B: K x N = (ker_h * ker_w * in_channels) x (out_h * out_w)
 		*/
-		int n, c;
-		int M = 1;
+		int n;
+		int M = out_chs;
 		int N = out_h * out_w;
 		int K = ker_h * ker_w * in_chs;
-		float* space = new float[K * N];
 
 		for (n = 0; n < batch; n++) {
-			float* B = space;
+			float* B = work_space;
 			const float* im = prev_out + in_h * in_w * in_chs * n;
 			im2col(im, in_chs, in_h, in_w, out_h, out_w, ker_h, pad, B);
-			for (c = 0; c < out_chs; c++) {
-				const float* A = kernel + c * K;
-				float* C = output + N * (c + out_chs * n);
-				gemm_nn(M, N, K, 1.0F, A, K, B, N, C, N);
-				//cblas_sgemm(CblasRowMajor, CblasNoTrans, CblasNoTrans, M, N, K, 1.0F, A, K, B, N, 0.0F, C, N);
-				add_bias(C, N, bias[c]);
-			}
-			set_zero(B, K * N);
+
+			const float* A = kernel;
+			float* C = output + M * N * n;
+
+			gemm_nn(M, N, K, 1.f, A, K, B, N, C, N);
+			add_bias(C, M, N, bias);
 		}
-		delete_memory(space);
 	}
 
 	void Conv2d::backward_propagate(const float* prev_out, float* prev_delta, bool isFirst)
 	{
 		/*
-		* A: M x K = 1 x (out_h x out_w)
+		* A: M x K = out_chs x (out_h x out_w)
 		* B: K x N = (out_h * out_w) x ( ker_h * ker_w) 
 		*/
 		int n, c1, c2;
-		int M = 1;
-		int N = ker_h * ker_w;
+		int M = out_chs;
+		int N = ker_h * ker_w * in_chs;
 		int K = out_h * out_w;
-		float* space = new float[K * N];
 
 		for (n = 0; n < batch; n++) {
-			for (c1 = 0; c1 < in_chs; c1++) {
-				float* B = space;
-				const float* im = prev_out + in_h * in_w * (c1 + in_chs * n);
-				im2col(im, 1, in_h, in_w, ker_h, ker_w, out_h, pad, B);
-				for (c2 = 0; c2 < out_chs; c2++) {
-					const float* A = delta + K * (c2 + out_chs * n);
-					float* C = dkernel + N * (c1 + in_chs * c2);
-					gemm_nn(M, N, K, 1.0F, A, K, B, N, C, N);
-					//cblas_sgemm(CblasRowMajor, CblasNoTrans, CblasNoTrans, M, N, K, 1.0F, A, K, B, N, 0.0F, C, N);
-				}
-				set_zero(B, K * N);
-			}
+			float* B = work_space;
+			const float* im = prev_out + in_h * in_w * in_chs * n;
+			im2col(im, in_chs, in_h, in_w, out_h, out_w, ker_h, pad, B);
+
+			const float* A = delta + M * K * n;
+			float* C = dkernel;
+
+			gemm_nt(M, N, K, 1.f, A, K, B, K, C, N);
 		}
 
 		for (n = 0; n < batch; n++) {
@@ -221,34 +223,28 @@ namespace simple_nn
 			}
 		}
 
-		delete_memory(space);
-
 		if (!isFirst) {
-			M = 1;
+			M = in_chs;
 			N = in_h * in_w;
 			K = ker_h * ker_w;
-			float* space1 = new float[K * N];
-			float* space2 = new float[K];
+			float* temp_space = new float[M * K];
 
 			int pad = ker_h - 1;
 			for (n = 0; n < batch; n++) {
 				for (c1 = 0; c1 < out_chs; c1++) {
-					float* B = space1;
+					float* B = work_space;
 					const float* im = delta + out_h * out_w * (c1 + out_chs * n);
 					im2col(im, 1, out_h, out_w, in_h, in_w, ker_h, pad, B);
+					float* A = temp_space;
 					for (c2 = 0; c2 < in_chs; c2++) {
-						float* A = space2;
-						float* tmp = kernel + K * (c2 + in_chs * c1);
-						std::reverse_copy(tmp, tmp + K, A);
-						float* C = prev_delta + N * (c2 + in_chs * n);
-						gemm_nn(M, N, K, 1.0F, A, K, B, N, C, N);
-						//cblas_sgemm(CblasRowMajor, CblasNoTrans, CblasNoTrans, M, N, K, 1.0F, A, K, B, N, 0.0F, C, N);
+						float* temp = kernel + K * (c2 + in_chs * c1);
+						std::reverse_copy(temp, temp + K, A + K * c2);
 					}
-					set_zero(B, K * N);
+					float* C = prev_delta + M * N * n;
+					gemm_nn(M, N, K, 1.f, A, K, B, N, C, N);
 				}
 			}
-			delete_memory(space1);
-			delete_memory(space2);
+			delete_memory(temp_space);
 		}
 	}
 
